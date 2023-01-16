5bab9649849a942f1ee36e9658e329edc785fa38
Kyj97 <gld66777@gmail.com>
2023-01-16 13:24:27 +0900

    save2

--- day03_homework.py
+++ day03_homework.py
@@ -5,6 +5,17 @@ That's - a moray!"""
 
 print(song.replace(' m', ' M'))
 
+#idx = song.rfind('m')
+#song2 = song.replace(song[idx], song[idx].upper())
+#print(song2.endswith('moray!'))
+
+song_list = song.split()
+print(song_list)
+song_list[14] = song_list[14].title()
+song_string = ' '.join(song_list)
+print(song_string)
+
+
 #5.2
 #Q: question
 #A: answer
@@ -19,12 +30,24 @@ answers = [
     "No, I'm a frayed kont.",
     "'Pop!' goes the weasel."
 ]
+print(f'Q:{questions[0]}')
+print(f'A:{answers[0]}')
+print(f'Q:{questions[1]}')
+print(f'A:{answers[1]}')
+print(f'Q:{questions[2]}')
+print(f'A:{answers[2]}')
+
+
 
 #5.3
 print("""My kitty cat likes %s,
 My kitty cat likes %s,
 My kitty cat fell on his %s And now thinks he's a %s.""" %('roast beef', 'ham', 'head', 'clam'))
+print("My kitty cat likes %s,\nMy kitty cat likes %s,\nMy kitty cat fell on his %s And now thinks he's a %s." %('roast beef', 'ham', 'head', 'clam'))
 
+
+
+#5.4
 letter = """Dear {salutation} {name},
 Thank you for your letter. We are sorry that our {product} {verbed} in your {room}. Please note that it should never be used in a {room},especially near any {animals}.
 
@@ -35,7 +58,27 @@ Sincerely,
 {spokesman}
 {job_title}"""
 
+#5.5
 
 
 
 print(letter)
+
+#5.6
+duck = 'duck'
+gourd = 'gourd'
+spitz = 'spitz'
+print("%sy Mc%sface" % (duck, duck))
+print("%sy Mc%sface" % (gourd, gourd))
+print("%sy Mc%sface" % (spitz, spitz))
+
+#5.7
+print("{0}y Mc{0}face" .format (duck, gourd, spitz))
+print("{1}y Mc{1}face" .format (duck, gourd, spitz))
+print("{2}y Mc{2}face" .format (duck, gourd, spitz))
+
+
+#5.8
+print(f"{duck}y Mc{duck}face")
+print(f"{gourd}y Mc{gourd}face")
+print(f"{spitz}y Mc{spitz}face")
